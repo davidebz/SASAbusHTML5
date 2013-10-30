85ec29f80726d90a7bf873db2c195a860045e130
davide <d@vide.bz>
2013-10-30 17:56:29 +0100

    Unused imports

--- src/it/bz/tis/sasabus/html5/shared/ui/HomePanel.java
+++ src/it/bz/tis/sasabus/html5/shared/ui/HomePanel.java
@@ -20,8 +20,6 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 package it.bz.tis.sasabus.html5.shared.ui;
 
 import it.bz.tis.sasabus.html5.shared.ui.map.SASAbusMap;
-import bz.davide.dmweb.server.Html5ParserServerSide;
-import bz.davide.dmweb.shared.model.Div;
 import bz.davide.dmweb.shared.view.DivView;
 import bz.davide.dmweb.shared.view.PageChangeHandler;
 import bz.davide.dmweb.shared.view.TextNodeView;
