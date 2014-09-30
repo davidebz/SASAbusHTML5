a89ab82dab87152f27d5898fa92a3187a1c12db7
davide <d@vide.bz>
2014-09-30 21:22:43 +0200

    remove wrong line in war script

--- deploy/war.sh
+++ deploy/war.sh
@@ -12,7 +12,6 @@ rm -r $TMPWARDIR
 
 rsync -av --exclude .gitignore --exclude WEB-INF/deploy ../war/ $TMPWARDIR/
 rsync -av ../../DMWeb/war/WEB-INF/classes/ $TMPWARDIR/WEB-INF/classes/
-rsync -av ../../DMXmlJson/bin/ $TMPWARDIR/WEB-INF/classes/
 date "+%F %T" > $TMPWARDIR/timestamp.txt
 
 cd $TMPWARDIR
